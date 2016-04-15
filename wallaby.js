74cbb7ef4eb07e79ea5c491db58baea346e9ac98
John Papa <john@johnpapa.net>
2016-04-15 08:22:42 -0400

    wallaby refactor

--- wallaby.js
+++ wallaby.js
@@ -3,41 +3,22 @@ module.exports = function (wallaby) {
   var appBase = 'src/client/';
 
   return {
-    files: [{
-      pattern: 'node_modules/es6-shim/es6-shim.js',
-      instrument: false
-    }, {
-        pattern: 'node_modules/systemjs/dist/system-polyfills.js',
-        instrument: false
-      }, {
-        pattern: 'node_modules/systemjs/dist/system.js',
-        instrument: false
-      }, {
-        pattern: 'node_modules/reflect-metadata/Reflect.js',
-        instrument: false
-      }, {
-        pattern: 'node_modules/zone.js/dist/zone.js',
-        instrument: false
-      }, {
-        pattern: 'node_modules/zone.js/dist/long-stack-trace-zone.js',
-        instrument: false
-      }, {
-        pattern: 'node_modules/zone.js/dist/jasmine-patch.js',
-        instrument: false
-      },
+    files: [
+      { pattern: 'node_modules/es6-shim/es6-shim.js', instrument: false },
+      { pattern: 'node_modules/systemjs/dist/system-polyfills.js', instrument: false },
+      { pattern: 'node_modules/systemjs/dist/system.js', instrument: false },
+      { pattern: 'node_modules/reflect-metadata/Reflect.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/zone.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/long-stack-trace-zone.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/jasmine-patch.js', instrument: false },
 
-      {
-        pattern: appBase + '+(app|api)/**/*+(ts|html|css)',
-        load: false
-      }, {
-        pattern: appBase + 'app/**/*.spec.ts',
-        ignore: true
-      }],
+      { pattern: appBase + '+(app|api)/**/*+(ts|html|css)', load: false },
+      { pattern: appBase + 'app/**/*.spec.ts', ignore: true }
+    ],
 
-    tests: [{
-      pattern: appBase + 'app/**/*.spec.ts',
-      load: false
-    }],
+    tests: [
+      { pattern: appBase + 'app/**/*.spec.ts', load: false }
+    ],
 
     compilers: {
       '**/*.ts': wallaby.compilers.typeScript(require('./src/client/tsconfig.json').compilerOptions)
@@ -76,7 +57,7 @@ module.exports = function (wallaby) {
         'shared/spinner',
         'shared/toast',
         'speakers'
-      ].reduce(function(barrelConfig, barrelName) {
+      ].reduce(function (barrelConfig, barrelName) {
         barrelConfig[appPackage + '/' + barrelName] = {
           format: 'register',
           defaultExtension: 'js',
@@ -100,11 +81,18 @@ module.exports = function (wallaby) {
 
       var promises = [];
 
-      Promise.all([System.import('angular2/testing'), System.import('angular2/platform/testing/browser')])
+      Promise.all([
+        System.import('angular2/testing'),
+        System.import('angular2/platform/testing/browser')
+      ])
         .then(function (results) {
           var testing = results[0];
           var browser = results[1];
-          testing.setBaseTestProviders(browser.TEST_BROWSER_PLATFORM_PROVIDERS, browser.TEST_BROWSER_APPLICATION_PROVIDERS);
+
+          testing.setBaseTestProviders(
+            browser.TEST_BROWSER_PLATFORM_PROVIDERS,
+            browser.TEST_BROWSER_APPLICATION_PROVIDERS
+          );
 
           for (var i = 0, len = wallaby.tests.length; i < len; i++) {
             promises.push(System['import'](wallaby.tests[i].replace(/\.js$/, '')));

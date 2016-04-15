cc4319713eb9d8b865fdc38d346d3c22af1cb49a
John Papa <john@johnpapa.net>
2016-04-14 23:42:03 -0400

    button test

---
+++ src/client/app/+dashboard/dashboard-button/dashboard-button.component.spec.ts
@@ -0,0 +1,52 @@
+/* tslint:disable:no-unused-variable */
+import { DashboardButtonComponent } from './dashboard-button.component';
+
+import {
+  it,
+  iit,
+  xit,
+  describe,
+  ddescribe,
+  xdescribe,
+  expect,
+  fakeAsync,
+  tick,
+  beforeEach,
+  inject,
+  injectAsync,
+  withProviders,
+  beforeEachProviders,
+  TestComponentBuilder
+} from 'angular2/testing';
+
+import { provide }        from 'angular2/core';
+import { ViewMetadata }   from 'angular2/core';
+import { PromiseWrapper } from 'angular2/src/facade/promise';
+
+/////////// Module Preparation ///////////////////////
+interface Done {
+  (): void;
+  fail: (err: any) => void;
+}
+
+////////  SPECS  /////////////
+
+
+describe('DashboardButtonComponent Smoke test', () => {
+  it('should run a passing test', () => {
+    expect(true).toEqual(true, 'should pass');
+  });
+});
+
+describe('DashboardButtonComponent', function () {
+  beforeEachProviders((): any => []);
+
+  it('should instantiate component',
+    injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+
+      return tcb.createAsync(DashboardButtonComponent).then(fixture => {
+        expect(fixture.componentInstance instanceof DashboardButtonComponent).toBe(true, 'should create DashboardButtonComponent');
+      });
+    }));
+
+});

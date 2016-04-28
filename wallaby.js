2ca15bdc90a86159329388a51d66f5e48f966cb8
Artem Govorov <artem.govorov@gmail.com>
2016-04-28 23:03:54 +1000

    chore: updated wallaby to use angular 2.0.0-beta.16

--- wallaby.js
+++ wallaby.js
@@ -7,6 +7,7 @@ module.exports = function (wallaby) {
       { pattern: 'node_modules/es6-shim/es6-shim.js', instrument: false },
       { pattern: 'node_modules/systemjs/dist/system-polyfills.js', instrument: false },
       { pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', instrument: false },
+      { pattern: 'node_modules/zone.js/dist/async-test.js', instrument: false },
       { pattern: 'node_modules/systemjs/dist/system.js', instrument: false },
       { pattern: 'node_modules/rxjs/bundles/Rx.js', instrument: false },
       { pattern: 'node_modules/angular2/bundles/angular2.dev.js', instrument: false },
